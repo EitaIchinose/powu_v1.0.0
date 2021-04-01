de5e07f86725ed0df07107a88bf33e8ddf0ec02e
EitaIchinose <dellps.psdell@gmail.com>
2021-04-01 19:09:27 +0900

    コード微修正

--- app/javascript/plan_balloon.js
+++ app/javascript/plan_balloon.js
@@ -5,16 +5,15 @@ $(window).on('load', function() {
       var time = new Date(value.start_time)
       var nowTime = moment(time).format()
       var tooltipHtml = `
-      <div>
-        <div class="tooltipClose">×</div>
-          <div style="margin: 10px;">
-            <div>
-            予定：${value.title}<br>
-              ${nowTime}<br>
-            </div>
-          </div>
-        </div>
-      </div>`;
+        <div>
+					<div style="margin: 10px;">
+						<div>
+							予定：${value.title}<br>
+							開始：${moment(startTime).format('YYYY-MM-DD HH:mm')}<br>
+							終了：${moment(endTime).format('YYYY-MM-DD HH:mm')}<br>
+						</div>
+					</div>
+				</div>`;
 
         $(".showTooltip2-" + value.id).tooltip({
             content: tooltipHtml
